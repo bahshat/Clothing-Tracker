07498832871fd80513f4dbef471f9577032700e7
Rais Iqbal <bahshat@gmail.com>
2025-07-26 03:52:18 +0530

    Views added to possible paths

--- production_tracker/urls.py
+++ production_tracker/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import OrderListView, OrderDetailView, UpdateOrderStageView
+from .views import (
+    OrderListView, OrderDetailView, UpdateOrderStageView,
+    IndividualListView, MeasurementListView, VendorRoleListView, VendorListView,
+    PipelineStageListView, InvoiceListView
+)
 
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order_list'),
     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
     path('order-stage/<int:pk>/update/', UpdateOrderStageView.as_view(), name='update_order_stage'),
+    path('individuals/', IndividualListView.as_view(), name='individual_list'),
+    path('measurements/', MeasurementListView.as_view(), name='measurement_list'),
+    path('vendor-roles/', VendorRoleListView.as_view(), name='vendorrole_list'),
+    path('vendors/', VendorListView.as_view(), name='vendor_list'),
+    path('pipeline-stages/', PipelineStageListView.as_view(), name='pipelinestage_list'),
+    path('invoices/', InvoiceListView.as_view(), name='invoice_list'),
 ]
--- production_tracker/views.py
+++ production_tracker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
-from .models import Order, OrderStage
+from .models import Order, OrderStage, Individual, Measurement, VendorRole, Vendor, PipelineStage, Invoice
 from .forms import OrderStageUpdateForm
 from datetime import date
 
@@ -30,10 +30,11 @@ class UpdateOrderStageView(View):
                 updated_stage.end_date = date.today()
                 
                 # Get the next stage in the sequence
+                # Assuming stages are ordered by their ID for simplicity in this example
                 next_stage = OrderStage.objects.filter(
                     order=updated_stage.order, 
-                    stage__id=updated_stage.stage.id + 1
-                ).first()
+                    stage__id__gt=updated_stage.stage.id
+                ).order_by('stage__id').first()
 
                 if next_stage:
                     next_stage.status = 'In Progress'
@@ -41,3 +42,33 @@ class UpdateOrderStageView(View):
 
             updated_stage.save()
         return redirect('order_detail', pk=order_stage.order.pk)
+
+class IndividualListView(ListView):
+    model = Individual
+    template_name = 'production_tracker/individual_list.html'
+    context_object_name = 'individuals'
+
+class MeasurementListView(ListView):
+    model = Measurement
+    template_name = 'production_tracker/measurement_list.html'
+    context_object_name = 'measurements'
+
+class VendorRoleListView(ListView):
+    model = VendorRole
+    template_name = 'production_tracker/vendorrole_list.html'
+    context_object_name = 'vendor_roles'
+
+class VendorListView(ListView):
+    model = Vendor
+    template_name = 'production_tracker/vendor_list.html'
+    context_object_name = 'vendors'
+
+class PipelineStageListView(ListView):
+    model = PipelineStage
+    template_name = 'production_tracker/pipelinestage_list.html'
+    context_object_name = 'pipeline_stages'
+
+class InvoiceListView(ListView):
+    model = Invoice
+    template_name = 'production_tracker/invoice_list.html'
+    context_object_name = 'invoices'
