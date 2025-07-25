2b962ba7bd4c694877c895e6c90a1f2b00c57fee
Rais Iqbal <bahshat@gmail.com>
2025-07-26 03:09:54 +0530

    Postgresql connection added

--- production_tracker/admin.py
+++ production_tracker/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import (
+    Individual, Measurement, VendorRole, Vendor, PipelineStage, Order, OrderStage, Invoice
+)
 
-# Register your models here.
+class OrderStageInline(admin.TabularInline):
+    model = OrderStage
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'customer', 'order_date', 'status')
+    inlines = [OrderStageInline]
+
+@admin.register(Individual)
+class IndividualAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role')
+
+admin.site.register(Measurement)
+admin.site.register(VendorRole)
+admin.site.register(PipelineStage)
+admin.site.register(OrderStage)
+admin.site.register(Invoice)
